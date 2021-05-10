cc5362598e9402c2e4de6292202d98a82fdfe590
fowwo <54595444+fowwo@users.noreply.github.com>
2021-05-10 17:59:08 -0500

    Add option generator

---
+++ generate.js
@@ -0,0 +1,40 @@
+var settings = document.getElementById("settings");
+var active = {};
+
+function addHeading(text) {
+	let heading = document.createElement("h1");
+	heading.innerHTML = text;
+	settings.appendChild(heading);
+}
+function addSection(name, property, description = "") {
+	let heading = document.createElement("h3");
+	let p = document.createElement("p");
+	heading.innerHTML = name;
+	p.innerHTML = description;
+
+	let container = document.createElement("div");
+	container.className = "color-option-container";
+
+	let defaultOption = createColorOption(`var(--${property})`);
+	defaultOption.classList.add("default", "active");
+	active[property] = defaultOption;
+	container.appendChild(defaultOption);
+	for (var i = 0; i < 10; i++) {
+		let option = createColorOption(`var(--p${i})`);
+		container.appendChild(option);
+	}
+
+	settings.appendChild(heading);
+	settings.appendChild(p);
+	settings.appendChild(container);
+}
+function createColorOption(color) {
+	let option = document.createElement("div");
+	option.innerHTML = "✓";
+	option.className = "color-option";
+	option.style.backgroundColor = color;
+	return option;
+}
+function hr() {
+	settings.appendChild(document.createElement("hr"));
+}
