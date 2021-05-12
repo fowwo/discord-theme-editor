6d3bb25e6f9769026dcd30d9c11f50f11be558a4
fowwo <54595444+fowwo@users.noreply.github.com>
2021-05-12 16:34:28 -0500

    Handle every color representation
    'getPropertyValue' gets the RGB or RGBA representation of any color, and that can be converted to hexadecimal.

--- index.js
+++ index.js
@@ -39,73 +39,17 @@ function showColorInfo(property) {
 	let container = document.getElementById("palette-container");
 	container.style.visibility = "visible";
 	container.style.opacity = "100%";
-	let value = getComputedStyle(document.documentElement).getPropertyValue(`--${property}`).trim();
-	document.getElementById("selected-color").style.backgroundColor = `var(--${property})`;
-	let hex = "";
-	let rgba = [];
-	if (value.startsWith("#")) {
-		hex = value;
-		rgba = hexToRGBA(hex);
-	} else {
-		let arr = value.split("(")[1];
-		rgba = arr.substring(0, arr.length - 1).split(",").map((x) => parseFloat(x.trim()));
-		if (value.startsWith("hsl")) {
-			rgba[1] /= 100;
-			rgba[2] /= 100;
-			rgba = HSLAToRGBA(rgba);
-		}
-		hex = `#${RGBAToHex(rgba)}`;
-	}
-	document.getElementById("hex-input").value = hex.substring(1);
+	let selectedColor = document.getElementById("selected-color");
+	selectedColor.style.backgroundColor = `var(--${property})`;
+	let rgba = getComputedStyle(selectedColor).getPropertyValue("background-color").split("(")[1].split(")")[0].split(",").map((x) => parseFloat(x.trim()));
+	document.getElementById("hex-input").value = RGBAToHex(rgba);
 	document.getElementById("r-value").innerHTML = rgba[0];
 	document.getElementById("g-value").innerHTML = rgba[1];
 	document.getElementById("b-value").innerHTML = rgba[2];
+	if (rgba.length === 3) rgba[3] = 1;
 	document.getElementById("a-value").innerHTML = rgba[3].toFixed(2);
 }
 
-/**
- * Converts a hexadecimal representation of a color
- * into an RGBA representation.
- * @param {String} hex - The hexadecimal color.
- */
-function hexToRGBA(hex) {
-	if (hex.startsWith("#")) hex = hex.substring(1);
-	let m = { '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15 };
-	if (hex.length === 3) return [ 17 * m[hex[0]], 17 * m[hex[1]], 17 * m[hex[2]], 1 ];
-	if (hex.length === 4) return [ 17 * m[hex[0]], 17 * m[hex[1]], 17 * m[hex[2]], m[hex[3]] / 15 ];
-	if (hex.length === 6) return [ 16 * m[hex[0]] + m[hex[1]], 16 * m[hex[2]] + m[hex[3]], 16 * m[hex[4]] + m[hex[5]], 1 ];
-	if (hex.length === 8) return [ 16 * m[hex[0]] + m[hex[1]], 16 * m[hex[2]] + m[hex[3]], 16 * m[hex[4]] + m[hex[5]], (16 * m[hex[6]] + m[hex[7]]) / 255 ];
-}
-
-/**
- * Converts an HSLA representation of a color
- * into an RGBA representation.
- * @param {Number[]} hsla - The HSLA color.
- */
-function HSLAToRGBA(hsla) {
-	let c = (1 - Math.abs(2 * hsla[2] - 1)) * hsla[1];
-	let x = c * (1 - Math.abs((hsla[0] / 60) % 2 - 1));
-	let m = hsla[2] - c / 2;
-	let arr = [];
-	if (hsla[0] < 60) {
-		arr = [ c, x, 0 ];
-	} else if (hsla[0] < 120) {
-		arr = [ x, c, 0 ];
-	} else if (hsla[0] < 180) {
-		arr = [ 0, c, x ];
-	} else if (hsla[0] < 240) {
-		arr = [ 0, x, c ];
-	} else if (hsla[0] < 300) {
-		arr = [ x, 0, c ];
-	} else {
-		arr = [ c, 0, x ];
-	}
-	let r = Math.round(255 * (arr[0] + m));
-	let g = Math.round(255 * (arr[1] + m));
-	let b = Math.round(255 * (arr[2] + m));
-	if (hsla.length === 3) return [ r, g, b ];
-	return [ r, g, b, hsla[3] ];
-}
 
 /**
  * Converts an RGBA representation of a color
