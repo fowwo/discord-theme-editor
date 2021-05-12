2fdd9227b8547b9f4508693ef9573ed6e4e406a1
fowwo <54595444+fowwo@users.noreply.github.com>
2021-05-11 19:36:48 -0500

    Allow RGBA and HSLA colors to be shown as current color

--- index.js
+++ index.js
@@ -39,9 +39,23 @@ function showColorInfo(property) {
 	let container = document.getElementById("palette-container");
 	container.style.visibility = "visible";
 	container.style.opacity = "100%";
-	let hex = getComputedStyle(document.documentElement).getPropertyValue(`--${property}`).trim();
-	let rgba = hexToRGBA(hex);
+	let value = getComputedStyle(document.documentElement).getPropertyValue(`--${property}`).trim();
 	document.getElementById("selected-color").style.backgroundColor = `var(--${property})`;
+	let hex = "";
+	let rgba = [];
+	if (value.startsWith("#")) {
+		hex = value;
+		rgba = hexToRGBA(hex);
+	} else {
+		let arr = value.split("(")[1];
+		rgba = arr.substring(0, arr.length - 1).split(",").map((x) => parseFloat(x.trim()));
+		if (value.startsWith("hsl")) {
+			rgba[1] /= 100;
+			rgba[2] /= 100;
+			rgba = HSLAToRGBA(rgba);
+		}
+		hex = `#${RGBAToHex(rgba)}`;
+	}
 	document.getElementById("hex-input").value = hex.substring(1);
 	document.getElementById("r-value").innerHTML = rgba[0];
 	document.getElementById("g-value").innerHTML = rgba[1];
@@ -61,6 +75,51 @@ function hexToRGBA(hex) {
 	if (hex.length === 4) return [ 17 * m[hex[0]], 17 * m[hex[1]], 17 * m[hex[2]], m[hex[3]] / 15 ];
 	if (hex.length === 6) return [ 16 * m[hex[0]] + m[hex[1]], 16 * m[hex[2]] + m[hex[3]], 16 * m[hex[4]] + m[hex[5]], 1 ];
 	if (hex.length === 8) return [ 16 * m[hex[0]] + m[hex[1]], 16 * m[hex[2]] + m[hex[3]], 16 * m[hex[4]] + m[hex[5]], (16 * m[hex[6]] + m[hex[7]]) / 255 ];
+}
+
+/**
+ * Converts an HSLA representation of a color
+ * into an RGBA representation.
+ * @param {Number[]} hsla - The HSLA color.
+ */
+function HSLAToRGBA(hsla) {
+	let c = (1 - Math.abs(2 * hsla[2] - 1)) * hsla[1];
+	let x = c * (1 - Math.abs((hsla[0] / 60) % 2 - 1));
+	let m = hsla[2] - c / 2;
+	let arr = [];
+	if (hsla[0] < 60) {
+		arr = [ c, x, 0 ];
+	} else if (hsla[0] < 120) {
+		arr = [ x, c, 0 ];
+	} else if (hsla[0] < 180) {
+		arr = [ 0, c, x ];
+	} else if (hsla[0] < 240) {
+		arr = [ 0, x, c ];
+	} else if (hsla[0] < 300) {
+		arr = [ x, 0, c ];
+	} else {
+		arr = [ c, 0, x ];
+	}
+	let r = Math.round(255 * (arr[0] + m));
+	let g = Math.round(255 * (arr[1] + m));
+	let b = Math.round(255 * (arr[2] + m));
+	if (hsla.length === 3) return [ r, g, b ];
+	return [ r, g, b, hsla[3] ];
+}
+
+/**
+ * Converts an RGBA representation of a color
+ * into an hexadecimal representation.
+ * @param {Number[]} rgba - The RGBA color.
+ */
+function RGBAToHex(rgba) {
+	let m = { 0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'a', 11: 'b', 12: 'c', 13: 'd', 14: 'e', 15: 'f' };
+	let r = "" + m[Math.floor(rgba[0] / 16)] + m[rgba[0] % 16];
+	let g = "" + m[Math.floor(rgba[1] / 16)] + m[rgba[1] % 16];
+	let b = "" + m[Math.floor(rgba[2] / 16)] + m[rgba[2] % 16];
+	let a = "";
+	if (rgba.length === 4) a = "" + m[Math.floor(255 * rgba[3] / 16)] + m[Math.floor(255 * rgba[3] % 16)];
+	return `${r}${g}${b}${a}`;
 }
 
 /**
